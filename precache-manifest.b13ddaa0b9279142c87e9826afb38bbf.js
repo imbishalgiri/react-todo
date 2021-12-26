0aa260347454edaae800d59299708e4dfe002431
bishal_giri <bishal_giri@bishal-giris-MacBook-Air.local>
2021-12-26 21:53:05 +0545

    Updates

---
+++ precache-manifest.b13ddaa0b9279142c87e9826afb38bbf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e11fa6a99f9887a2e1d94d738e24f0f9",
+    "url": "/react-todo/index.html"
+  },
+  {
+    "revision": "7f7e9e819740c9a48c59",
+    "url": "/react-todo/static/css/main.f230a5ed.chunk.css"
+  },
+  {
+    "revision": "002ed7ce7e2c35737e62",
+    "url": "/react-todo/static/js/2.4536ff63.chunk.js"
+  },
+  {
+    "revision": "7f7e9e819740c9a48c59",
+    "url": "/react-todo/static/js/main.7e24a23e.chunk.js"
+  },
+  {
+    "revision": "052fad23ca9f036fcf47",
+    "url": "/react-todo/static/js/runtime~main.79666361.js"
+  },
+  {
+    "revision": "d323cc696e2f5f48c6125d535bf6ebd5",
+    "url": "/react-todo/static/media/heading-image.d323cc69.jpg"
+  }
+]);
