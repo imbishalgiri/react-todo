cca2a9f7d5a6cf03220207b5242a27b049131ebb
bishal_giri <bishal_giri@bishal-giris-MacBook-Air.local>
2021-12-27 16:17:22 +0545

    finished writing html and css to the site, still responsiveness gotta be worked out

--- src/components/icons/Trash.jsx
+++ src/components/icons/Trash.jsx
@@ -1,9 +1,6 @@
-import React from 'react';
-import './trash.styles.css';
+import React, { useContext } from "react";
+import "./trash.styles.css";
 
-
-const Trash = () => (
-    <div className="list-icons icon icon-cross"></div>
-)
+const Trash = ({ id }) => <div className="list-icons icon icon-cross"></div>;
 
 export default Trash;
